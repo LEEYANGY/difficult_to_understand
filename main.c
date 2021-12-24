cb533690c79757d136b615defa27b45f216f76de
LEEYANGY <1787294587@qq.com>
2021-12-25 03:30:41 +0800

    一维数组

--- main.c
+++ main.c
@@ -1,30 +1,31 @@
 /**
  * 语法使用c11进行学习。
- * 如下：程序是十进制转成二进制
+ * 数组-一维数组
  */
 #include <stdio.h>
-
-void to_binary(unsigned long number);
+#define Mouths 12
 
 int main(void) {
-    unsigned long number;
-    printf("Enter an integer (q to quit):\n");
-    while (scanf("%lu",&number)==1){
-        printf("Binary equivalent: ");
-        to_binary(number);
-        putchar('\n');
-        printf("Enter an integer (q to quit):\n");
+//    数组
+//    打印每个月的天数
+//    方法一，给数组附上初始值
+    int days1[Mouths]={31,28,31,30,31,30,31,31,30,31,30,31};
+    for(int index=0;index<Mouths;index++){
+        printf("Mouths %2d has %2d days.\n",index+1,days1[index]);
     }
-    printf("Done.\n");
-    return 0;
-}
+    printf("\n");
+//    方法二，不赋初始值
+    int days2[]={31,28,31,30,31,30,31,31,30,31,30,31};
+    for(int index=0;index<Mouths;index++){
+        printf("Mouths %2d has %2d days.\n",index+1,days2[index]);
+    }
+    printf("\n");
+    //    方法二，不赋初始值
+    int days3[]={31,28,31,30,31,30,31,31,30,31,30,31};
+//    sizeof days3是整个数组的大小，sizeof days3[0]是第一个数组的下标,表示第一个数组大小，整个数组的大小除于一个即为数组长度
+    for(int index=0;index<(sizeof days3/sizeof days3[0]);index++){
+        printf("Mouths %2d has %2d days.\n",index+1,days3[index]);
+    }
 
-void to_binary(unsigned long number) {
-    int r;
-    r=number%2;
-    if(number>=2) {
-        to_binary(number / 2);
-    }
-        putchar(r==0? '0':'1');
-    return;
+    return 0;
 }
