eba5764568a48d4814f55afc4a5fcb19abe47088
LEEYANGY <1787294587@qq.com>
2022-01-14 23:36:32 +0800

    文件1

---
+++ file_1.c
@@ -0,0 +1,19 @@
+﻿//
+// Created by LEEYANGY on 2022/1/14.
+//
+
+#include "stdio.h"
+#include "stdlib.h"
+
+int main(int argc , char *argv[]){
+
+    int ch;
+    FILE *fp;
+    fp = fopen("2","r");
+    ch = getc(fp);
+    while (ch != EOF){
+        putchar(ch);
+        ch = getc(fp);
+    }
+    return 0;
+}
