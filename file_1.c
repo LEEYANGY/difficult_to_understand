ff338842a4dadf96a2a847a7db008ce77abbf516
LEEYANGY <1787294587@qq.com>
2022-01-14 23:39:05 +0800

    代码优化

--- file_1.c
+++ file_1.c
@@ -6,14 +6,16 @@
 #include "stdlib.h"
 
 int main(int argc , char *argv[]){
-
+//    用int ch接收EOF
     int ch;
+//    文件指针
     FILE *fp;
+//    打开一个文件
     fp = fopen("2","r");
-    ch = getc(fp);
-    while (ch != EOF){
+//    打印文件内容
+    while ( (ch = getc(fp) ) != EOF){
+//        将文件内容输出到屏幕
         putchar(ch);
-        ch = getc(fp);
     }
     return 0;
 }
