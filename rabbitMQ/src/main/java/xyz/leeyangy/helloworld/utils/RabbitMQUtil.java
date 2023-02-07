b33c9d649875ec5bae90c866674f45109ed7d7d4
LEEYANGYANG <1787294587@qq.com>
2023-02-08 05:54:08 +0800

    工具类代码优化

--- rabbitMQ/src/main/java/xyz/leeyangy/helloworld/utils/RabbitMQUtil.java
+++ rabbitMQ/src/main/java/xyz/leeyangy/helloworld/utils/RabbitMQUtil.java
@@ -13,20 +13,26 @@ import com.rabbitmq.client.Connection;
  */
 public class RabbitMQUtil {
 
+    //  创建连接工厂
+    private static ConnectionFactory connectionFactory;
+
+    static {
+//        静态代码块,在类加载时候执行一次(因为开销特别大,故写到静态代码块中)
+        connectionFactory = new ConnectionFactory();
+//            设置主机
+        connectionFactory.setHost("localhost");
+//          设置端口
+        connectionFactory.setPort(5672);
+//           设置虚拟机
+        connectionFactory.setVirtualHost("/null");
+//           设置用户名和密码
+        connectionFactory.setUsername("null");
+        connectionFactory.setPassword("null");
+    }
+
     //    定义提供连接对象方法
     public static Connection getConnection() {
         try {
-//            创建连接工厂
-            ConnectionFactory connectionFactory = new ConnectionFactory();
-//            设置主机
-            connectionFactory.setHost("localhost");
-//          设置端口
-            connectionFactory.setPort(5672);
-//           设置虚拟机
-            connectionFactory.setVirtualHost("/null");
-//           设置用户名和密码
-            connectionFactory.setUsername("null");
-            connectionFactory.setPassword("null");
 //            建立连接
             return connectionFactory.newConnection();
         } catch (Exception e) {
