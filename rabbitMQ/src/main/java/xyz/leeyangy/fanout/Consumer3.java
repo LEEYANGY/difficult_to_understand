b5e68dd4b704a6990422f3f4d45da0bd063d9c66
LEEYANGYANG <1787294587@qq.com>
2023-02-10 03:04:24 +0800

    广播模型

--- rabbitMQ/src/main/java/xyz/leeyangy/fanout/Consumer3.java
+++ rabbitMQ/src/main/java/xyz/leeyangy/fanout/Consumer3.java
@@ -1,4 +1,4 @@
-package xyz.leeyangy.helloworld;
+package xyz.leeyangy.fanout;
 
 import com.rabbitmq.client.*;
 import xyz.leeyangy.utils.RabbitMQUtil;
@@ -9,11 +9,11 @@ import java.util.concurrent.TimeoutException;
 /**
  * @Author liyangyang
  * @Date: 2023/02/08/5:06
- * @Package xyz.leeyangy.helloworld
+ * @Package xyz.leeyangy.fanout
  * @Version 1.0
  * @Description: 消费者获取消息
  */
-public class Consumer {
+public class Consumer3 {
 
     /**
      * @Param: []
@@ -28,23 +28,29 @@ public class Consumer {
         Connection connection = RabbitMQUtil.getConnection();
 //        创建通道
         Channel channel = connection.createChannel();
-//        绑定消息队列
-        channel.queueDeclare("hello", false, false, false, null);
+//        绑定交换机
+        channel.exchangeDeclare("register","fanout");
+
+//        临时队列
+        String tempQueue = channel.queueDeclare().getQueue();
+
+//        绑定交换机队列
+        channel.queueBind(tempQueue,"register","");
 
 //        消费消息
 //        参数1: 消费那个队列消息
 //        参数2: 开始消息的自动确认机制
 //        参数3: 消费时回调接口
-        channel.basicConsume("hello", true, new DefaultConsumer(channel) {
+        channel.basicConsume(tempQueue, true, new DefaultConsumer(channel) {
             @Override
 //            最后一个参数:消息队列中取出消息
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
-                System.out.println(new String(body));
+                System.out.println("消费消息："+new String(body));
             }
         });
 
 //        关闭连接
-        RabbitMQUtil.closeConnectionAndChannel(channel, connection);
+//        RabbitMQUtil.closeConnectionAndChannel(channel, connection);
     }
 
 }
---
+++ rabbitMQ/src/main/java/xyz/leeyangy/fanout/Provider.java
@@ -0,0 +1,58 @@
+package xyz.leeyangy.fanout;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import org.junit.Test;
+import xyz.leeyangy.utils.RabbitMQUtil;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @Author liyangyang
+ * @Date: 2023/02/08/4:24
+ * @Package xyz.leeyangy.fanout
+ * @Version 1.0
+ * @Description: 生产者代码
+ */
+public class Provider {
+
+    /**
+     * @Param: []
+     * @return: void
+     * @Author: liyangyang
+     * @Date: 2023/2/8 4:26
+     * @Description: 生产消息代码
+     */
+    @Test
+    public void testProvider() throws IOException, TimeoutException {
+//        创建连接
+        Connection connection = RabbitMQUtil.getConnection();
+//        获取连接通道
+        Channel channel = connection.createChannel();
+
+//        将声明通道指定交换机
+//        参数1：交换机名称
+//        参数2：交换机类型  fanout 广播类型
+//        参数3：
+//        参数4：
+        channel.exchangeDeclare("register","fanout");
+//        绑定对应消息队列
+//        参数1： 队列名称，如果不存在，则自动创建
+//        参数2：用来定义队列特性是否要持久化 true  持久化队列  false 不持久化
+//        参数3：是否独占队列 true 独占，false 不独占
+//        参数4： 是否在消费完成后自动删除队列 true 删除  false 不删除
+//        参数5：额外附加参数
+//        channel.queueDeclare("hello", false, false, false, null);
+
+//        发布消息
+//        参数1： 交换机名称
+//        参数2： 路由键
+//        参数3： 消息额外设置
+//        参数4: 消息具体内容
+        channel.basicPublish("register", "", null, "fanout type message".getBytes());
+
+//        关闭连接
+        RabbitMQUtil.closeConnectionAndChannel(channel, connection);
+    }
+}
