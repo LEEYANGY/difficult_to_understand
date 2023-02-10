ce946d606b672f5daef58857de02d91968020f4e
LEEYANGYANG <1787294587@qq.com>
2023-02-11 05:24:14 +0800

    文件合并

--- mybatis/src/main/java/com/leeyangy/dao/IUserDao.java
+++ mybatis/src/main/java/com/leeyangy/dao/IUserDao.java
@@ -1,6 +1,7 @@
 package com.leeyangy.dao;
 
 import com.leeyangy.domain.User;
+import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 /**
@@ -12,6 +13,7 @@ public interface IUserDao {
      *查询所有操作
      * @return
      */
+    @Select("select * from user")
     List<User> findAll();
 }
 //此处创建的操作需要映射到Resource目录下的 同当前结构目录的IUserDao.xml内
