ff577fa0373640c3e13f26ce05d8b926f64ea7a0
LEEYANGYANG <1787294587@qq.com>
2023-03-30 12:27:57 +0800

    启动主程序

--- security-springboot/src/main/java/xyz/leeyangy/SecuritySpringbootApplication.java
+++ security-springboot/src/main/java/xyz/leeyangy/SecuritySpringbootApplication.java
@@ -1,13 +1,17 @@
 package xyz.leeyangy;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
+@MapperScan("xyz.leeyangy.mapper")
 public class SecuritySpringbootApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(SecuritySpringbootApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(SecuritySpringbootApplication.class, args);
+        System.out.println(run);
     }
 
 }
