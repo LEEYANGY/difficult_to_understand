1338178bc1e949b546b0398568b049c646c454c6
LEEYANGYANG <1787294587@qq.com>
2023-03-31 23:17:04 +0800

    初始化scala项目环境，并添加测试对象进行测试环境是否搭建正常

---
+++ scala/src/main/scala/xyz/leeyangy/aaa.scala
@@ -0,0 +1,14 @@
+package xyz.leeyangy
+
+/**
+ * @Author liyangyang
+ * @Date: 2023/03/31 23:13
+ * @Package xyz.leeyangy
+ * @Version 1.0
+ * @Description: 测试环境是否正常了  jdk 需要 11
+ */
+object aaa {
+  def main(args: Array[String]): Unit = {
+    System.out.print("Hello Scala")
+  }
+}
