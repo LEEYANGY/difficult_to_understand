f68da0f1f17c61a934507a8db09d19d0aca5937e
LEEYANGYANG <1787294587@qq.com>
2023-05-02 04:17:20 +0800

    测试类...

--- back-end/student-back-end/src/test/java/org/dragonwings/StudentApplicationTest.java
+++ back-end/student-back-end/src/test/java/org/dragonwings/StudentApplicationTest.java
@@ -1,43 +1,55 @@
 package org.dragonwings;
 
+import org.dragonwings.school.modular.system.entity.Chats;
+import org.dragonwings.school.modular.system.service.ChatsService;
 import org.junit.jupiter.api.Test;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.data.redis.core.RedisTemplate;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
 
 import javax.annotation.Resource;
-import javax.sql.DataSource;
-import java.sql.SQLException;
+
 
 /**
  * Unit test for simple App.
  */
 @SpringBootTest
+@MapperScan("org.dragonwings.school.modular.system.mapper")
 public class StudentApplicationTest {
 
-    @Resource
-    private RedisTemplate<String,String> redisTemplate;
+//    @Resource
+//    private RedisTemplate<String,String> redisTemplate;
 
 //    @Resource
 
+//
+//    @Resource
+//    private DataSource dataSource;
+//
+//
+//    @Test
+//    void testMysql() throws SQLException {
+//        System.out.println(dataSource);
+//        System.out.println(dataSource.getConnection());
+//    }
+//
+//    @Test
+//    void testGetEncodePassword(){
+//        String pass = "123321456";
+////        redisTemplate.ge
+//        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+//        final String passHas = encoder.encode(pass);
+//        System.out.println(passHas);
+//    }
 
-    @Resource
-    private DataSource dataSource;
 
-
-    @Test
-    void testMysql() throws SQLException {
-        System.out.println(dataSource);
-        System.out.println(dataSource.getConnection());
-    }
-
-    @Test
-    void testGetEncodePassword(){
-        String pass = "123321456";
-//        redisTemplate.ge
-        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
-        final String passHas = encoder.encode(pass);
-        System.out.println(passHas);
-    }
+//    @Resource
+//    private ChatsService chatsService;
+//
+//    @Test
+//    public void testGetContentByGid() {
+//        Chats chats = chatsService.findChatContentByGid(10000000L);
+//        System.out.println(chats);
+//    }
 
 }
