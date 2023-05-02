84d384ab399bdc2422d2e382625c72890f6424c0
LEEYANGYANG <1787294587@qq.com>
2023-05-02 19:14:16 +0800

    字段修改

--- back-end/student-back-end/src/main/java/org/dragonwings/school/modular/system/entity/Event.java
+++ back-end/student-back-end/src/main/java/org/dragonwings/school/modular/system/entity/Event.java
@@ -39,9 +39,19 @@ public class Event extends BaseUserTimeEntity {
     private String sponsor;
 
     /**
+     * 事件类型(0实习，1看病，2.探亲，3.培训，4.请假，5.其它)
+     */
+    private String eventType;
+
+    /**
      * 事件状态 (0正常，1停用)
      */
     private String status;
+
+    /**
+     * 删除标志(0代表未删除，1代表逻辑删除)
+     */
+    private Integer delFlag;
 
 
 }
