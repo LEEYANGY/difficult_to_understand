438e967e89f60e02bed86922058fcf4cf397f010
LEEYANGYANG <1787294587@qq.com>
2023-05-02 20:34:46 +0800

    完成自助审批申请

--- back-end/student-back-end/src/main/java/org/dragonwings/school/modular/system/controller/EventController.java
+++ back-end/student-back-end/src/main/java/org/dragonwings/school/modular/system/controller/EventController.java
@@ -1,9 +1,17 @@
 package org.dragonwings.school.modular.system.controller;
 
 
+import org.dragonwings.school.framework.respone.ResponseData;
+import org.dragonwings.school.modular.system.entity.Event;
+import org.dragonwings.school.modular.system.service.EventService;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -17,5 +25,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/system/event")
 public class EventController {
 
+
+
+    @Resource
+    EventService eventService;
+
+    @PostMapping("/cafeAdd")
+    public ResponseData cafeAdd(@RequestBody Event event){
+        boolean save = eventService.save(event);
+        if (!ObjectUtils.isEmpty(save)){
+            return ResponseData.success(200,"添加成功",null);
+        }
+        return ResponseData.error(500,"添加失败",null);
+    }
 }
 
