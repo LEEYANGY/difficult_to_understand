b31d74d8114b95b7d9edc714c565130816b93f55
LEEYANGYANG <1787294587@qq.com>
2023-05-07 19:51:49 +0800

    删除不需要的bean

--- back-end/student-back-end/src/main/java/org/dragonwings/school/modular/system/service/impl/UserServiceImpl.java
+++ back-end/student-back-end/src/main/java/org/dragonwings/school/modular/system/service/impl/UserServiceImpl.java
@@ -121,12 +121,12 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, User> implements Us
             user.setPassword(new BCryptPasswordEncoder().encode(user.getPassword()));
         }
         System.out.println(user.getPassword());
-//        修改用户数据
-        if (baseMapper.update(user, new QueryWrapper<User>().eq("id", user.getId())) > 0) {
-            return new ResponseResult<>(200, "更新成功", null);
-        } else {
+////        修改用户数据
+//        if (baseMapper.update(user, new QueryWrapper<User>().eq("id", user.getId())) > 0) {
+//            return new ResponseResult<>(200, "更新成功", null);
+//        } else {
             return new ResponseResult<>(500, "更新失败", null);
-        }
+//        }
     }
 
     /**
