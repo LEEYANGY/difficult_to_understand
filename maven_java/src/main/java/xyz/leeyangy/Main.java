70ee84090b9e0139581d327d204e1fec1d862f94
leeyangyangy <1787294587@qq.com>
2023-12-18 02:21:01 +0800

    maven之hello world

---
+++ maven_java/src/main/java/xyz/leeyangy/Main.java
@@ -0,0 +1,17 @@
+package xyz.leeyangy;
+
+/**
+ * @ProjectName : Default (Template) Project
+ * @Package : xyz.leeyangy
+ * @ClassName : ${NAME}
+ * @Author : leeyangy
+ * @CreateTime : 2023/12/18 2:07
+ * @Version : 1.0
+ * @Description :
+ * @Modify_log :
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
