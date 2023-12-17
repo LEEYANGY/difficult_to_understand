8a02aaeb93537c45ddada575eec498558408ace1
leeyangyangy <1787294587@qq.com>
2023-12-18 02:30:25 +0800

    添加Java web测试

---
+++ maven_web/src/main/java/xyz/leeyangy/maven/Main.java
@@ -0,0 +1,17 @@
+package xyz.leeyangy.maven;
+
+/**
+ * @ProjectName : Default (Template) Project
+ * @Package : xyz.leeyangy.maven
+ * @ClassName : ${NAME}
+ * @Author : leeyangy
+ * @CreateTime : 2023/12/18 2:22
+ * @Version : 1.0
+ * @Description :
+ * @Modify_log :
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
